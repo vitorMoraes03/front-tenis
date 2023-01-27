0caf1599b7365f34365151a04da4e556f0761a48
Vitor Moraes <vitoristvan@hotmail.com>
2023-01-27 14:54:37 -0300

    nav started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { NavBar } from "./components/NavBar";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    /* border: 1px solid red; */
+  }
+
+  html {
+        font-size: 62.5%;
+        --space-small: 1rem;
+    }
+`
 
 function App() {
   return (
+    <>
+    <GlobalStyle/>
     <div className="App">
-      teste new branch vitor
+      <NavBar></NavBar>
     </div>
+    </>
   );
 }
 
