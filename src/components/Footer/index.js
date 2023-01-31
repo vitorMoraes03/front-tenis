7190ed62584e20a95542bdaa296c1fa81512847d
Vitor Moraes <vitoristvan@hotmail.com>
2023-01-31 11:40:59 -0300

    general layout ready

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,114 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+    width: 100vw;
+`
+
+const StyledFooterMain = styled.div`
+    width: 100%;
+    display: flex;
+    padding: 7rem;
+    justify-content: space-between;
+`
+
+const StyledFooterCta = styled.div`
+    
+
+    h3 {
+        text-transform: uppercase;
+        margin-bottom: 1rem;
+    }
+
+    div {
+        display: flex;
+    }
+
+    button {
+
+    }
+
+    ion-icon {
+        
+    }
+`
+
+const StyledFooterUls = styled.div`
+
+`
+
+const StyledFooterList = styled.ul`
+
+`
+
+const StyledFooterMedia = styled.div`
+    
+
+    h3 {
+        margin-bottom: 1rem;
+    }
+
+    div {
+        display: flex;
+        gap: 1rem;
+        justify-content: center;
+    }
+
+    ion-icon {
+        
+    }
+
+
+
+`
+
+const StyledFooterBottom = styled.div`
+
+`
+
 export function Footer(){
     return (
-        <>
-        FOOTER
-        </>
+        <StyledFooter>
+            <StyledFooterMain>
+                <StyledFooterCta>
+                    <h3>Receba novidades</h3>
+                    <div>
+                    <button>Registre seu email</button>
+                    <ion-icon name="chevron-forward-outline"></ion-icon>
+                    </div>
+                </StyledFooterCta>
+                <StyledFooterUls>
+                    <StyledFooterList>
+                        <li>Garantia</li>
+                        <li>Contato</li>
+                        <li>FAQ</li>
+                        <li>Sobre</li>
+                        <li>Licença</li>
+                    </StyledFooterList>
+                </StyledFooterUls>
+                <StyledFooterMedia>
+                    <h3>Siga nas redes sociais</h3>
+                    <div>
+                        <ion-icon name="logo-instagram"></ion-icon>
+                        <ion-icon name="logo-linkedin"></ion-icon>
+                        <ion-icon name="logo-github"></ion-icon>
+                    </div>
+                </StyledFooterMedia>
+            </StyledFooterMain>
+
+            <StyledFooterBottom>
+                <div>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div>
+                    <p></p>
+                </div>
+                <div>
+                    <img></img>
+                </div>
+            </StyledFooterBottom>
+        </StyledFooter>
        
     )
 }
